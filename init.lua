4b5cf176e63d42bc53f091dba8e3292acc01ef67
jakecastelli <959672929@qq.com>
2022-03-17 22:08:27 +1030

    fix: rename neoscroll to scroll to avoid module namespace collision

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require 'user.treesitter'
 require 'user.cmp'
 require 'user.lsp'
 require 'user.impatient'
-require 'user.neoscroll'
+require 'user.scroll'
 
 -- vim.cmd('source ~/.config/nvim/core/plug.vim')
 -- legacy general config
