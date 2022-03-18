9ede04d217142b69701fedb185a21b7134682618
jakecastelli <959672929@qq.com>
2022-03-18 18:07:01 +1030

    feat: format on save by null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,4 +14,14 @@ null_ls.setup {
     formatting.prettier,
     formatting.stylua,
   },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd [[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]]
+    end
+  end,
 }
