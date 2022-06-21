2fac26f7ae23a596148b082e8332134bc00d1c76
jakecastelli <959672929@qq.com>
2022-06-21 23:52:38 +0930

    fix: update lsp to work with nvim 0.7

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -82,6 +82,11 @@ M.on_attach = function(client, bufnr)
   if client.name == 'tsserver' then
     client.resolved_capabilities.document_formatting = false
   end
+
+    if client.name == "sumneko_lua" then
+    client.resolved_capabilities.document_formatting = false
+  end
+
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -1,15 +1,17 @@
 return {
   settings = {
-
     Lua = {
       diagnostics = {
-        globals = { 'vim' },
+        globals = { "vim" },
       },
       workspace = {
         library = {
-          [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-          [vim.fn.stdpath 'config' .. '/lua'] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
         },
+      },
+      telemetry = {
+        enable = false,
       },
     },
   },
