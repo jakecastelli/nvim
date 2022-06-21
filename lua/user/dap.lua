ae4408ddde14bc1037f0894f114fd089913085ff
jakecastelli <959672929@qq.com>
2022-06-21 23:52:50 +0930

    feat: add dap with plugins

---
+++ lua/user/dap.lua
@@ -0,0 +1,80 @@
+local dap_status_ok, dap = pcall(require, 'dap')
+if not dap_status_ok then
+  return
+end
+
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = { os.getenv 'HOME' .. '/projects/vscode-node-debug2/out/src/nodeDebug.js' },
+}
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require('dap.utils').pick_process,
+  },
+}
+
+local dap_ui_status_ok, dapui = pcall(require, 'dapui')
+if not dap_ui_status_ok then
+  return
+end
+
+local dap_install_status_ok, dap_install = pcall(require, 'dap-install')
+if not dap_install_status_ok then
+  return
+end
+
+-- dap_install.setup {}
+-- dap_install.setup {
+-- installation_path = vim.fn.stdpath 'data' .. '/dapinstall/',
+-- installation_path = vim.fn.stdpath 'data' .. '/dapinstall/',
+-- }
+
+-- dap_install.config('python', {})
+-- add other configs here
+-- dap_install.config('jsnode', {})
+
+dapui.setup {
+  sidebar = {
+    elements = {
+      {
+        id = 'scopes',
+        size = 0.25, -- Can be float or integer > 1
+      },
+      { id = 'breakpoints', size = 0.25 },
+    },
+    size = 40,
+    position = 'left', -- Can be "left", "right", "top", "bottom"
+  },
+  tray = {
+    elements = {},
+  },
+}
+
+vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
+
+dap.listeners.after.event_initialized['dapui_config'] = function()
+  dapui.open()
+end
+
+dap.listeners.before.event_terminated['dapui_config'] = function()
+  dapui.close()
+end
+
+dap.listeners.before.event_exited['dapui_config'] = function()
+  dapui.close()
+end
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -81,3 +81,17 @@ keymap('v', '<leader>t', ':TranslateW<cr>', opts)
 -- Copy to system clipboard
 keymap('n', '<leader>c', '"*y', opts)
 keymap('v', '<leader>c', '"*y', opts)
+
+-- Open projects
+keymap('n', '<leader>p', "<cmd>lua require('telescope').extensions.projects.projects()<CR>", opts)
+
+-- DAP
+keymap('n', '<leader>db', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
+keymap('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>", opts)
+keymap('n', '<leader>di', "<cmd>lua require'dap'.step_into()<cr>", opts)
+keymap('n', '<leader>do', "<cmd>lua require'dap'.step_over()<cr>", opts)
+keymap('n', '<leader>dO', "<cmd>lua require'dap'.step_out()<cr>", opts)
+keymap('n', '<leader>dr', "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
+keymap('n', '<leader>dl', "<cmd>lua require'dap'.run_last()<cr>", opts)
+keymap('n', '<leader>du', "<cmd>lua require'dapui'.toggle()<cr>", opts)
+keymap('n', '<leader>dt', "<cmd>lua require'dap'.terminate()<cr>", opts)
